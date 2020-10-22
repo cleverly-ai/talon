b9b5096ec84be884779b99c7de8a6c45612cf5a7
Pedro Coelho <punnie@ethereal.io>
2020-10-22 19:03:08 +0100

    Convert to poetry

--- setup.py
+++ setup.py
@@ -1,29 +1,6 @@
-from __future__ import absolute_import
-from setuptools import setup, find_packages
-from setuptools.command.install import install
+#!/usr/bin/env python
 
+import setuptools
 
-setup(
-    name="talon",
-    version="1.5.1",
-    description=("Mailgun library " "to extract message quotations and signatures."),
-    long_description=open("README.rst").read(),
-    author="Mailgun Inc.",
-    author_email="admin@mailgunhq.com",
-    url="https://github.com/mailgun/talon",
-    license="APACHE2",
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    include_package_data=True,
-    zip_safe=True,
-    extras_require={"ml": ["numpy", "scipy", "scikit-learn",]},
-    install_requires=[
-        "lxml>=2.3.3",
-        "regex>=1",
-        "chardet>=1.0.1",
-        "cchardet>=0.3.5",
-        "cssselect",
-        "six>=1.10.0",
-        "html5lib",
-    ],
-    tests_require=["mock", "nose>=1.2.1", "coverage"],
-)
+if __name__ == "__main__":
+    setuptools.setup()
